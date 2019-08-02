fae7251bb0a7a8f409eee5ec9e98837f9d7df7fc
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-02 13:45:58 -0400

    simple web app

--- src/main/java/hbcu/stay/ready/SampleController.java
+++ src/main/java/hbcu/stay/ready/SampleController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloDoController {
+public class SampleController {
 
     @GetMapping(value="/sample")
     public String methodName() {
-        return FileReader.readFile("Sample.txt");
+        return FileReader.readFile("webapp/Sample.html");
     }
 }
